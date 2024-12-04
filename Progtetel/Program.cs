fd6b0fd76939befe480af8c8e8717b9e091e54a4
RedBall4 <kiss.mate2008@ckik.hu>
2024-12-04 11:01:46 +0100

    Feladat kezdete

--- Progtetel/Program.cs
+++ Progtetel/Program.cs
@@ -7,6 +7,25 @@ namespace Progtetel
         static void Main(string[] args)
         {
             // SIGMA
+
+            int[] x = { 7, 2, -5, 4, 11, 9 };
+
+            // Max kiválasztás
+
+            int n = x.Length;
+
+            int i = 0;
+            int maxe = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (x[i] > maxe)
+                {
+                    x[i] = x[maxe];
+                }
+            }
+            Console.WriteLine(maxe);
+
+            //Párosak kiválogatása
         }
     }
 }
